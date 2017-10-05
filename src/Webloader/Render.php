1f2c1fc7b875e7ad6c5ed8ea3b10f2b37305bcd8
Machy8 <8machy@seznam.cz>
2017-10-05 22:18:42 +0200

    - Updated render

--- src/Webloader/Render.php
+++ src/Webloader/Render.php
@@ -1,10 +1,15 @@
 <?php
+
 /**
- * Created by IntelliJ IDEA.
- * User: machy8
- * Date: 03.10.17
- * Time: 10:08
+ *
+ * Copyright (c) Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
  */
+
+declare(strict_types = 1);
 
 namespace WebLoader;
 
@@ -30,59 +35,81 @@ class Render
 	 */
 	private $version;
 
-	public function __construct(string $outputDir, int $version)
+
+	public function __construct(string $outputDir, string $version)
 	{
 		$this->outputDir = $outputDir;
 		$this->version = $version;
 	}
 
 
-	private function generateElement(string $collectionName, string $element, array $attributes, bool $content, string $fileExtension): string
-	{
-		$tag = '<' . $element;
-		$filePath = $this->outputDir . '/' . $collectionName . '.' . $fileExtension;
-		$isScriptElement = $element === self::SCRIPT_ELEMENT;
-
-		foreach ($attributes as $attribute => $value) {
-			$tag .= ' ' . $attribute . '="' . $value . '"';
-		}
-
-		if ($content) {
-			$fileContent = file_get_contents($filePath);
-			$tag .= ">\n" . $fileContent . "\n";
-
-			if ($isScriptElement) {
-				$tag .= '</script>';
-
-			} else {
-				$tag .= '</style>';
-			}
-
-		} elseif ($element === self::LINK_ELEMENT) {
-			$tag .= ' href="' . $filePath . '?v=' . $this->version . '">';
-
-		} elseif ($isScriptElement) {
-			$tag .= 'src="' . $filePath . '?v= ' . $this->version . '"></script>';
-		}
-
-		return $tag;
-	}
-
-
 	public function css(string $collectionName, array $attributes = NULL, bool $content = FALSE): string
 	{
 		$attributes = $attributes ?? [];
-		$element = $content ? self::STYLE_ELEMENT : self::LINK_ELEMENT;
+		$element = self::STYLE_ELEMENT;
+
+		if ( ! $content) {
+			$element = self::LINK_ELEMENT;
+			$attributes['rel'] = 'stylesheet';
+			$attributes['type'] = 'text/css';
+		}
+
 		return $this->generateElement($collectionName, $element, $attributes, $content, Compiler::CSS);
 	}
 
 
 	public function js(string $collectionName, array $attributes = NULL, bool $content = FALSE): string
 	{
+		$attributes = $attributes ?? [];
+		$attributes['type'] = 'text/javascript';
 
-		$attributes= $attributes ?? [];
-		$element = $content ? self::STYLE_ELEMENT : self::LINK_ELEMENT;
-		return $this->generateElement($collectionName, $element, $attributes, $content, Compiler::JS);
+		return $this->generateElement($collectionName, self::SCRIPT_ELEMENT, $attributes, $content, Compiler::JS);
+	}
+
+
+	private function generateElement(
+		string $collectionName,
+		string $element, array
+		$attributes,
+		bool $content,
+		string $fileExtension
+	): string {
+		$tag = '<' . $element;
+		$filePath = $this->outputDir . '/' . $collectionName . '.' . $fileExtension;
+		$isScriptElement = $element === self::SCRIPT_ELEMENT;
+
+		if ( ! $content) {
+			if ($element === self::LINK_ELEMENT) {
+				$attributes['href'] = $filePath . '?v=' . $this->version;
+
+			} elseif ($isScriptElement) {
+				$attributes['src'] = $filePath . '?v=' . $this->version;
+			}
+		}
+
+		foreach ($attributes as $attribute => $value) {
+			$tag .= ' ' . $attribute;
+
+			if ($value !== TRUE) {
+				$tag .= '="' . $value . '"';
+			}
+		}
+
+		$tag .= ">";
+
+		if ($content) {
+			$tag .= "\n" . file_get_contents($filePath) . "\n";
+
+			if ($element === self::STYLE_ELEMENT) {
+				$tag .= '</style>';
+			}
+		}
+
+		if ($isScriptElement) {
+			$tag .= '</script>';
+		}
+
+		return $tag;
 	}
 
 }
