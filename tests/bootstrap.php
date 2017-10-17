d6bb5f1914c6521db13ea48ecdbcf3eeab8c43f0
Machy8 <8machy@seznam.cz>
2017-10-17 17:17:36 +0200

    - Fixed tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/AbstractTestCase.php';
 
 Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
